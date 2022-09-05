6be51eb71d9993de8313a6e312640c28e6dcdd6c
Tarcísio Lima <tarcisio_thallys@hotmail.com>
2022-09-05 16:27:03 -0300

    close order updates.

--- backend/src/app/utils/closeOrder.js
+++ backend/src/app/utils/closeOrder.js
@@ -5,6 +5,7 @@ import Cashier from '../models/Cashier';
 import CashierOperation from '../models/CashierOperation';
 import TableOrder from '../models/TableOrder';
 import Table from '../models/Table';
+import { Op } from 'sequelize';
 
 const closeOrder = async (order) => {
     const cashier = await Cashier.findAll();
@@ -26,18 +27,36 @@ const closeOrder = async (order) => {
         attributes: ['gathered_tables', 'table_id']
       })
 
-    // LIBERAR MESA PRINCIPAL
-    await Table.update({status: 'free'}, { where: { id:  orderedTable.getDataValue('table_id')}});
-    
-    // LIBERAR OUTRAS MESAS
-    const united_tables = orderedTable.getDataValue('gathered_tables');
+    // LIBERAR MESA PRINCIPAL: se não houver outras comandas na mesma mesa
+    const otherOrderInTable = await TableOrder.findAll({
+        where: {
+        table_id: orderedTable.table_id,
+        order_id: {[Op.ne]: order.id}  
+      },
+      include: [{
+        model: Order,
+        as: 'order',
+        where: {
+          status: {[Op.ne]: 'paid'}
+        }
+      }]
+    });
 
-    if(united_tables){
-      const table_list = united_tables.split(',');
-      table_list.forEach(async (_table_id) => {        
-          await Table.update({status: 'free'}, { where: { id: _table_id}});        
-      });
+
+    if(otherOrderInTable.length === 0) {
+      await Table.update({status: 'free'}, { where: { id:  orderedTable.getDataValue('table_id')}});
+
+      // LIBERAR OUTRAS MESAS
+      const united_tables = orderedTable.getDataValue('gathered_tables');
+
+      if(united_tables){
+        const table_list = united_tables.split(',');
+        table_list.forEach(async (_table_id) => {        
+            await Table.update({status: 'free'}, { where: { id: _table_id}});        
+        });
+      }
     }
+   
 
     let total = orderProducts[0].products.reduce((acc, curr) => {
         return acc + (curr.sell_price * curr.order_product.quantity)
